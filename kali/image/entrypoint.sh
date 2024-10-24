4fb56673d064b3d705e79d389293fd019426d264
Corian (Cory) Kennedy <clkennedy@gmail.com>
2024-10-24 18:58:57 -0500

    Add files via upload

--- kali/image/entrypoint.sh
+++ kali/image/entrypoint.sh
@@ -6,8 +6,11 @@ set -e
 
 python http_server.py > /tmp/server_logs.txt 2>&1 &
 
-cd kali/
-STREAMLIT_SERVER_PORT=8501 python -m streamlit run streamlit.py > /tmp/streamlit_stdout.log &
+# Add environment variable to disable Streamlit's telemetry prompt
+cd /home/hakcer/kali
+export STREAMLIT_BROWSER_GATHER_USAGE_STATS=false
+export STREAMLIT_SERVER_GATHER_USAGE_STATS=false
+STREAMLIT_SERVER_PORT=8501 python -m streamlit run ./hakc_streamlit.py > /dev/null 2>&1 &
 
 echo "✨ k(a)l(i)is ready!"
 echo "➡️  Open http://localhost:8080 in your browser to begin"
